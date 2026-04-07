e1831afd7306da8c658b75aedd9bbd9f2daaa245
Thiya Senanayake <101445056+Thiya18@users.noreply.github.com>
2026-04-07 11:59:06 +0530

    Initial Commit

---
+++ EventFlow/EventFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventFlowApp.swift
+//  EventFlow
+//
+//  Created by Thiya on 2026-04-07.
+//
+
+import SwiftUI
+
+@main
+struct EventFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
